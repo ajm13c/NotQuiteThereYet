f221770ad0487560d22fbe10147cc62846ddb30c
ea11c <eric_rico2011@yahoo.com>
2016-11-29 17:22:25 -0500

    added basic maps activity
    code after permissions check doesn't execute

--- FacebookExample/app/src/main/java/com/example/fixit/facebookexample/LoginActivity.java
+++ FacebookExample/app/src/main/java/com/example/fixit/facebookexample/LoginActivity.java
@@ -382,7 +382,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+                Intent intent = new Intent(LoginActivity.this, MapsActivity.class);
+                startActivity(intent);
+//                finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
---
+++ FacebookExample/app/src/main/java/com/example/fixit/facebookexample/MapsActivity.java
@@ -0,0 +1,81 @@
+package com.example.fixit.facebookexample;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    Marker myLocation = null;
+    LocationManager locationManager;
+    LocationProvider provider;
+    String prov;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng tallahassee = new LatLng(30.44, -84.29);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(tallahassee, 14));
+        myLocation = mMap.addMarker(new MarkerOptions().position(tallahassee).title("You might be here"));
+        //TODO: update map to current location and place marker there
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        //Not sure why this never runs...
+        Toast.makeText(this, "Trying to get location", Toast.LENGTH_SHORT).show();
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        provider = locationManager.getProvider(locationManager.getAllProviders().get(0));
+        prov = String.valueOf(provider);
+        if(prov != null) {
+            Location location = locationManager.getLastKnownLocation(prov);
+            LatLng current = new LatLng(location.getLatitude(), location.getLongitude());
+            myLocation = mMap.addMarker(new MarkerOptions().position(current).title("Here you are!"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(current));
+        }
+
+        mMap.setMyLocationEnabled(true);
+    }
+}
