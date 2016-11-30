032ce12f751eb51e66efb476fe7a196197525873
ea11c <eric_rico2011@yahoo.com>
2016-11-30 14:54:56 -0500

    removed onclick listener, added location listener

--- FacebookExample/app/src/main/java/com/example/fixit/facebookexample/MapsActivity.java
+++ FacebookExample/app/src/main/java/com/example/fixit/facebookexample/MapsActivity.java
@@ -1,8 +1,10 @@
 package com.example.fixit.facebookexample;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
 import android.support.v4.app.ActivityCompat;
@@ -20,7 +22,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener, GoogleMap.OnInfoWindowClickListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
 
     private GoogleMap mMap;
     Marker myLocation = null;
@@ -55,7 +57,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(tallahassee, 14));
-        mMap.setOnMapLongClickListener(this);
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -67,11 +68,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             return;
         }
         //mMap.setMyLocationEnabled(true);
-        lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         provider = lm.getBestProvider(new Criteria(), true);
-        if(provider != null){
+        if (provider != null) {
             loc = lm.getLastKnownLocation(provider);
-            if(loc != null){
+            if (loc != null) {
                 LatLng current = new LatLng(loc.getLatitude(), loc.getLongitude());
                 myLocation = mMap.addMarker(new MarkerOptions()
                         .position(current)
@@ -79,30 +80,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(current));
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(current, 14));
-            }
-            else{
+            } else {
                 myLocation = mMap.addMarker(new MarkerOptions()
-                                            .position(tallahassee)
-                                            .title("Self")
-                                            .snippet("Default value")
-                                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+                        .position(tallahassee)
+                        .title("Self")
+                        .snippet("Default value")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
             }
         }
         mMap.setOnInfoWindowClickListener(this);
-    }
-
-    @Override
-    public void onMapLongClick(LatLng latLng) {
-        loc = lm.getLastKnownLocation(provider);
-        /*
-        * This is why it crashes ^^^ Use this method:
-        *
-        LocationManager locManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0, new LocationListener() {
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
             public void onLocationChanged(Location location) {
+                mMap.clear();
                 // Called when a new location is found by the network location provider.
                 //Here we put the code which will A) Update the location of all the other users
                 //B) Will upload your current location to the database
+                /*
+                loc = location
+                add code to push and pull from database
+                int num_results = SQL query to get all users with X+ interests in common
+                float distance;
+                Location target = new Location("");
+                for(int i = 0; i < num_results; i++){
+                    set target latitude and longitude according to query results
+                    distance = loc.distanceTo(target);
+                    if(distance < Some arbitrary constant distance){  //means target has enough common likes and is in range
+                   Marker temp = mMap.addMarker(new MarkerOptions().position(pos_from_db).title(name_from_db).snippet(common_interests))
+                }
+         }
+        */
             }
 
             public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -114,45 +120,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onProviderDisabled(String provider) {
             }
         });
-        * */
-        //clear all markers from map, then add relevant markers back.
-        mMap.clear();
-        if(loc != null) {
-            myLocation = mMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(loc.getLatitude(), loc.getLongitude()))
-                    .title("self")
-                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
-        }
-        else{
-            Log.i("loc", "Unable to get current location, using past value or default");
-        }
-        /*
-        add code to push and pull from database
-        int num_results = SQL query to get all users with X+ interests in common
-        float distance;
-        Location target = new Location("");
-        for(int i = 0; i < num_results; i++){
-                set target latitude and longitude according to query results
-                distance = loc.distanceTo(target);
-                if(distance < Some arbitrary constant distance){  //means target has enough common likes and is in range
-                   Marker temp = mMap.addMarker(new MarkerOptions().position(pos_from_db).title(name_from_db).snippet(common_interests))
-                }
-         }
-         */
-        //Example add marker only to test InfoWindowClick
-        LatLng arr[] = new LatLng[2];
-        arr[0] = latLng;
-        arr[1] = new LatLng(30.44, -84.29);
-        for(int i = 0; i < 2; i++) {
-            Marker temp = mMap.addMarker(new MarkerOptions().position(arr[i]).title("Testing" + i).snippet("info"));
-        }
     }
 
     @Override
     public void onInfoWindowClick(Marker marker) {
         if(marker.getTitle().equals("Self")){
             Log.i("self", "Clicked self");
-            return;
+            
         }
         else
             Log.i("other", "Clicked " + marker.getTitle());
